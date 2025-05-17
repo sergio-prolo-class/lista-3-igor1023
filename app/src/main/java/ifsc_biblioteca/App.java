a612aaba69c8e12637983ba0a4e20984acca9310
igor <igor.s10@aluno.ifsc.edu.br>
2025-05-17 16:33:59 -0300

    Iniciada implementação da questão 3. Não terminei ainda.

---
+++ app/src/main/java/ifsc_biblioteca/App.java
@@ -0,0 +1,11 @@
+package ifsc_biblioteca;
+
+public class App{
+
+    public static void main(String[] args){
+
+        
+
+    }
+
+}
--- app/src/main/java/ifsc_biblioteca/Biblioteca.java
+++ app/src/main/java/ifsc_biblioteca/Biblioteca.java
@@ -2,10 +2,6 @@ package ifsc_biblioteca;
 
 public class Biblioteca{
 
-    public static void main(String[] args){
-
-        System.out.println("Hello World");
-
-    }
+    
 
 }
