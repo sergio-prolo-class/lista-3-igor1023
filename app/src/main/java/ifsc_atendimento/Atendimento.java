880d7b078d00f04ee50e8c94dd760d688b8c40d0
igor <igor.s10@aluno.ifsc.edu.br>
2025-05-17 14:07:14 -0300

    Implementada questão 2. Ainda não realizei testes.

--- app/src/main/java/ifsc_atendimento/Atendimento.java
+++ app/src/main/java/ifsc_atendimento/Atendimento.java
@@ -1,11 +1,82 @@
 package ifsc_atendimento;
 
+import java.util.*;
+
 public class Atendimento{
 
-    public static void main(String[] args){
+    // Constantes
+    private final int TAMANHO_RELATORIO = 3;
 
-        System.out.println("Hello World");
+    // Atributos
+    private Queue<Cliente> filaClientes = new LinkedList<Cliente>();    
+    private List<Cliente> clientesRegistrados = new LinkedList<Cliente>();
+    private List<Cliente> clientesJaAtendidos = new LinkedList<Cliente>();    
+    private int[] relatorio = new int[TAMANHO_RELATORIO];
+
+    public void adicionarClienteNaFila(Cliente cliente){
+
+        filaClientes.add(cliente);
+        this.relatorio[2]++;
+        // incremento solicitacoes em espera
 
     }
 
+    public void registarSolicitacao(Cliente cliente){
+
+        clientesRegistrados.add(cliente);
+        this.relatorio[0]++;
+        // incremento solicitacoes registradas
+
+    }
+
+    public List<String> telefoneClientesRegistrados(){
+
+        List<String> telefone = new LinkedList<String>();
+
+        for(Cliente tel : this.clientesRegistrados)
+            telefone.add(tel.getTelefone());
+
+        return telefone;
+    }
+
+    public String nomeProximoCliente(){
+
+        return filaClientes.peek().getNome();
+
+    }
+
+    public String atenderProximoCliente(){
+
+        this.relatorio[1]++; // incremento solicitacoes atendidas
+        this.relatorio[2]--; // diminuo solicitacoes em espera
+        return filaClientes.remove().getNome();
+
+    }
+
+    public List<String> nomeClientesAtendidos(){
+
+        List<String> clientesAtendidos = new LinkedList<String>();
+
+        for(Cliente cli : clientesJaAtendidos){
+
+            clientesAtendidos.add(cli.getNome());
+
+        }
+
+        return clientesAtendidos;
+
+    }
+
+    public List<String> telefonesClientesEspera(){
+
+        List<String> telefoneClienteEspera = new LinkedList<String>();
+
+        for(Cliente cli : filaClientes){
+
+            telefoneClienteEspera.add(cli.getTelefone());
+
+        }
+
+        return telefoneClienteEspera;
+    }
 }
